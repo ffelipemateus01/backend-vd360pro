c35319a26a979bd2494b33dcaaca3cdf4fbf33eb
Felipe Mateus <fmfoliveira.dev@gmail.com>
2025-07-04 11:57:43 -0300

    Update sivec

--- api/io/sivecplus/controller.py
+++ api/io/sivecplus/controller.py
@@ -1,6 +1,9 @@
 from serial import Serial
 from serial import SerialException
 from api.io.sivecplus.repository import add_data, clear_data
+from api.io.sivecplus.converter import ConvertData
+from api.io.sivecplus.models import Data, DataFormatted
+from api.io.sivecplus.calibration import ElectrodeCalibration, GyroscopeCalibration
 from core.config import SerialConfigs
 import threading
 import time
@@ -11,6 +14,9 @@ class SerialController:
         self.serial_lock = threading.Lock()
         self.recording = False
         self.running = False
+        self.electrode_calibration = ElectrodeCalibration()
+        self.gyroscope_calibration = GyroscopeCalibration()
+        self.converter = ConvertData(self.electrode_calibration, self.gyroscope_calibration)
 
     def open(self, port_name: str) -> Serial | None:
         try:
@@ -28,6 +34,23 @@ class SerialController:
             print(f'Houve um problema ao tentar conectar com a serial. {e}')
             raise SerialException(f'Houve um problema ao tentar conectar com a serial. {e}')
 
+    def data_to_data_class(self, data: str) -> Data | None:
+        try:
+            data_parts = data.split(',')
+            values = {}
+            for part in data_parts:
+                key, value = part.split(':', 1)
+                values[key] = value
+            data_cls = Data(electrode_1=values['A1'],
+                            electrode_2=values['A2'],
+                            electrode_3=values['A3'],
+                            gyroscope_x=values['GX'],
+                            gyroscope_y=values['GY'],
+                            gyroscope_z=values['GZ'])
+            return data_cls
+        except Exception as e:
+            raise ValueError(f'Problema na conversão dos dados recebidos. {e}')
+
     def run(self, port_name: str):
         self.running = True
         print('Escutando...')
@@ -37,15 +60,18 @@ class SerialController:
                 with self.serial_lock:
                     if (my_serial.in_waiting > 0):
                         data=my_serial.readline().decode('utf-8').strip()
-                        print(f'Dado lido: {data}')
                         if self.recording:
                             add_data(data)
+                        #print(f'Dado lido: {data}')
+                        data_cls = self.data_to_data_class(data)
+                        data_formatted = self.converter.convert_data(data_cls, self.electrode_calibration, self.gyroscope_calibration)
+                        print(f'Data Convertido: {data_formatted}')
                         if not self.running:
                             break        
-                    time.sleep(0.500)
+                    time.sleep(1)
             except Exception as e:
-                print(f'Houve um problema ao tentar conectar com a serial. {e}')
-                raise SerialException(f'Houve um problema ao tentar conectar com a serial. {e}')
+                print(f'Houve um problema ao tentar ler os dados da serial. {e}')
+                raise SerialException(f'Houve um problema ao tentar ler os dados da serial. {e}')
         if port_name in self.connection:
             self.connection.pop(port_name)
         self.running = False
--- api/io/sivecplus/converter.py
+++ api/io/sivecplus/converter.py
@@ -1,18 +1,22 @@
 from api.io.sivecplus.models import Data, DataFormatted
 from api.io.sivecplus.calibration import ElectrodeCalibration, GyroscopeCalibration
-import json
-
-electrode_calibration = ElectrodeCalibration()
-gyroscope_calibration = GyroscopeCalibration()
 
 class ConvertData:
-    def convert_data(data: Data) -> DataFormatted | None:
+    def __init__(self, electrode_calibration: ElectrodeCalibration, gyroscope_calibration: GyroscopeCalibration):
+        self.electrode_calibration = electrode_calibration
+        self.gyroscope_calibration = gyroscope_calibration
+
+    def convert_data(self, data: Data, electrode_calibration: ElectrodeCalibration, gyroscope_calibration: GyroscopeCalibration) -> DataFormatted | None:
         data_electrode_1 = (data.electrode_1 - electrode_calibration.base_line_1) * electrode_calibration.bytes_by_degrees_1
         data_electrode_2 = (data.electrode_2 - electrode_calibration.base_line_2) * electrode_calibration.bytes_by_degrees_2
         data_electrode_3 = (data.electrode_3 - electrode_calibration.base_line_3) * electrode_calibration.bytes_by_degrees_3
         data_gyroscope_x = (data.gyroscope_x - gyroscope_calibration.offset_x) / gyroscope_calibration.convert_constant
         data_gyroscope_y = (data.gyroscope_y - gyroscope_calibration.offset_y) / gyroscope_calibration.convert_constant
         data_gyroscope_z = (data.gyroscope_z - gyroscope_calibration.offset_z) / gyroscope_calibration.convert_constant
-        data_formatted = DataFormatted(data_electrode_1, data_electrode_2, data_electrode_3, data_gyroscope_x, data_gyroscope_y, data_gyroscope_z)
-        print(f'Data Formatted: {json.dumps(data_formatted)}')
+        data_formatted = DataFormatted(g_electrode_1=data_electrode_1,
+                                       g_electrode_2=data_electrode_2,
+                                       g_electrode_3=data_electrode_3,
+                                       g_gyroscope_x=data_gyroscope_x,
+                                       g_gyroscope_y=data_gyroscope_y,
+                                       g_gyroscope_z=data_gyroscope_z)
         return data_formatted
